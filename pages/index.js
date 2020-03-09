cb36b29c556a0d8ff5199e697f9516564058bb86
Jean Pierre <stick-master666@hotmail.com>
2020-03-09 08:08:47 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Layout from '../components/Layout';
+
+const Home = () => {
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+export default Home
